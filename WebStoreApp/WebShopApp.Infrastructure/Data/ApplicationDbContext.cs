864a7e146dda97d55ed32b761715a9db08520220
themselfagain <sheikysmar7y@gmail.com>
2023-12-04 11:19:01 +0200

    Migracii batence

--- WebStoreApp/WebShopApp.Infrastructure/Data/ApplicationDbContext.cs
+++ WebStoreApp/WebShopApp.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using System.Diagnostics;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+
+using WebShopApp.Infrastructure.Data.Domain;
 
 namespace WebStoreApp.Data
 {
@@ -8,7 +12,11 @@ namespace WebStoreApp.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-            Database.EnsureCreated();
+           
         }
+        public DbSet<Brand> Brands { get; set; } 
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
